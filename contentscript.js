d99ebf21395b55046af5685005810652b5d65ea1
Thierry de LANGERON <tdelangeron@shivacom.fr>
2016-11-17 18:48:29 +0100

    First Commit

---
+++ contentscript.js
@@ -0,0 +1,8 @@
+//
+// Chrome Extension by bosswhale
+// (c) November 2016
+console.log("LeMondefr cleaner startup...");
+$("div.OUTBRAIN").hide();
+$("div#liste_reactions").hide();
+$("div#ultimedia_wrapper").hide();
+$("div#groupe_services").hide();
